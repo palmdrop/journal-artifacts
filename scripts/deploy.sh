1b5e877e5ec36181a2994e5a06ded89ff3672649
palmdrop <anton@exlex.se>
2023-01-20 08:21:57 +0100

    Add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,8 @@
+npm run build
+rm -r ../palmdrop.github.io/artifacts
+cp -r dist ../palmdrop.github.io/artifacts
+
+cd ../palmdrop.github.io
+git add artifacts
+git commit -m "Updated Artifacts"
+git push
